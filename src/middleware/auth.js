4724be04952678388bff20cd72f644376aca195f
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-04-15 11:31:45 +0530

    new commit-2

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const verifyToken = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.newToken;
 
-    // console.log(token);
+    console.log(token);
     if (!token)
       return res
         .status(400)
